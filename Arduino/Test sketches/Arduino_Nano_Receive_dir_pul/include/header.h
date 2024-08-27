dac2064bf2dda69ac81493f6e2ccdfe8d914ae2f
Luís Portela <78928841+LuisOPortela@users.noreply.github.com>
2024-08-27 15:16:47 +0100

    Add files via upload

---
+++ Arduino/Test sketches/Arduino_Nano_Receive_dir_pul/include/header.h
@@ -0,0 +1,25 @@
+#include <iostream>
+using  namespace std;
+
+struct moveoJoint
+{
+
+    int pulseRev;
+    int direction;
+    int pinPulse;
+    int pinDir;
+    AccelStepper motor;
+  
+    moveoJoint(int step, int dir, int pPulse, int pDir)
+     : pulseRev(step), direction(dir), pinPulse(pPulse), pinDir(pDir)
+    {
+     
+           
+        if (direction != 1 && direction != -1) 
+        {
+            std::cerr << "Warning: Invalid direction value. Setting to default (1)." << std::endl;
+            direction = 1;  // Set to default value
+        }   
+        motor = AccelStepper(AccelStepper::DRIVER, pinPulse, pinDir);
+    }   
+};
---
+++ Arduino/Test sketches/Arduino_Nano_Receive_dir_pul/src/main.cpp
@@ -0,0 +1,91 @@
+#include <Arduino.h>
+#include <AccelStepper.h>
+#include "header.h"
+
+
+//----------------------------------------------------------------
+//                 Definitions, Globals and Macros
+//     -  Pins
+//     -  Serial
+//     -  Motor
+//
+//----------------------------------------------------------------
+
+
+moveoJoint joint_1(1600,1,A0,A1);
+
+
+const byte maxLength = 10;
+char inputBuffer[maxLength];
+byte bufferIndex = 0;
+
+//void decode();
+
+int speed2steps(float speed, int pulsesRev);
+
+//----------------------------------------------------------------
+//                         Setup
+//     - Pins
+//     - Serial
+//     - Motor?
+//
+//----------------------------------------------------------------
+
+void setup() {
+  // put your setup code here, to run once:
+  
+  joint_1.motor.setMaxSpeed(1600);
+  joint_1.motor.setSpeed(800);
+  
+  Serial.begin(9600);
+
+}
+
+
+//----------------------------------------------------------------
+//                         Loop
+//     
+//     - Read Serial Port
+//     - Decode Message
+//     - Update values
+//
+//----------------------------------------------------------------
+void loop() 
+{
+
+  while (Serial.available() > 0) {
+    char receivedChar = Serial.read();
+    
+    // If newline is received, process the buffer
+    if (receivedChar == 's') {
+      inputBuffer[bufferIndex] = '\0';  // Null-terminate the string
+      float speed = atof(inputBuffer);    // Convert buffer to integer
+      int steps_sec = speed2steps(speed,joint_1.pulseRev);
+      
+      // Set the speed of the stepper motor
+      joint_1.motor.setSpeed(steps_sec); 	//The desired constant speed in steps per second. Positive is clockwise. Speeds of more than 1000 steps per second are unreliable
+      Serial.println(String("Steps per second:")+steps_sec);
+      // Reset buffer index for next input
+      bufferIndex = 0;
+    } else {
+      // Add received character to buffer if it does not exceed maxLength
+      if (bufferIndex < maxLength - 1) {
+        inputBuffer[bufferIndex++] = receivedChar;
+      }
+    }
+  }
+
+  // Run the motor at the set speed
+  joint_1.motor.runSpeed();
+}
+
+int speed2steps(float speed, int pulsesRev)
+{
+return int(speed*float(pulsesRev)/(2*PI)); //check
+}
+
+
+
+//void decode()
+
+
