9f8319a04d160dd7a6a436c3f4ae721d39c19ea5
Luís Portela <78928841+LuisOPortela@users.noreply.github.com>
2024-08-27 15:26:12 +0100

    Add files via upload

---
+++ Backups/Moveo-2-main(git_11_jun)/arm_try_3/launch/spawn_controllers.launch.py
@@ -0,0 +1,7 @@
+from moveit_configs_utils import MoveItConfigsBuilder
+from moveit_configs_utils.launches import generate_spawn_controllers_launch
+
+
+def generate_launch_description():
+    moveit_config = MoveItConfigsBuilder("moveo_urdf", package_name="arm_try_3").to_moveit_configs()
+    return generate_spawn_controllers_launch(moveit_config)
