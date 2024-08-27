0bb558002e19212d39ee4dd43776c410a7a50bdb
Luís Portela <78928841+LuisOPortela@users.noreply.github.com>
2024-08-27 15:24:54 +0100

    Delete arm_try_3 directory

--- arm_try_3/launch/rsp.launch.py
+++
@@ -1,7 +0,0 @@
-from moveit_configs_utils import MoveItConfigsBuilder
-from moveit_configs_utils.launches import generate_rsp_launch
-
-
-def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("moveo_urdf", package_name="arm_try_3").to_moveit_configs()
-    return generate_rsp_launch(moveit_config)
--- arm_try_3/launch/static_virtual_joint_tfs.launch.py
+++
@@ -1,7 +0,0 @@
-from moveit_configs_utils import MoveItConfigsBuilder
-from moveit_configs_utils.launches import generate_static_virtual_joint_tfs_launch
-
-
-def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("moveo_urdf", package_name="arm_try_3").to_moveit_configs()
-    return generate_static_virtual_joint_tfs_launch(moveit_config)
